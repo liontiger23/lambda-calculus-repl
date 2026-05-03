6a1c7caf536264a3b4ade4ccf1f7a0c1f65ab4c9
Ivan Trepakov <liontiger23@gmail.com>
2026-05-03 16:53:23 +0700

    Add options and depth setting

--- src/LambdaCalculus/REPL.hs
+++ src/LambdaCalculus/REPL.hs
@@ -12,8 +12,22 @@ import qualified Data.Map as M
 import Data.Map (Map)
 import Data.Char (isSpace)
 import Control.Exception (IOException, try)
+import Text.Read (readMaybe)
 
-type REPL = StateT (Map Ident Term) (InputT IO)
+-- * Options
+
+type Defs = Map Ident Term
+data REPLOptions = REPLOptions
+  { replDefs :: Defs
+  , replDepth :: Int
+  }
+
+defaultOptions :: REPLOptions
+defaultOptions = REPLOptions M.empty 100
+
+-- * REPL
+
+type REPL = StateT REPLOptions (InputT IO)
 
 replMain :: IO ()
 replMain = runREPL $
@@ -21,7 +35,7 @@ replMain = runREPL $
 
 runREPL :: REPL a -> IO a
 runREPL r = runInputT defaultSettings { historyFile = Just ".lambda-history" } $
-  evalStateT r M.empty
+  evalStateT r defaultOptions
 
 -- Runs the Run-Evaluate-Print-Loop
 repl :: String -> REPL ()
@@ -36,11 +50,30 @@ repl prompt =
   command "q" = pure ()
   command "quit" = pure ()
   command "defs" = printDefs *> repl prompt
+  command "depth" = printDepth *> repl prompt
   command cmd = (case break isSpace cmd of
       ("save", name) -> writeDefs (trim name)
       ("load", name) -> readDefs (trim name)
+      ("depth", dep) -> case readMaybe dep of
+        Just d -> putDepth d *> printDepth
+        Nothing -> lift $ outputStrLn ("Unexpected depth value: '" <> dep <> "'")
       _              -> lift $ outputStrLn ("Unknown command: " ++ cmd)
     ) *> repl prompt
+
+getDepth :: REPL Int
+getDepth = gets replDepth
+
+putDepth :: Int -> REPL ()
+putDepth d = modify (\o -> o { replDepth = d })
+
+getDefs :: REPL Defs
+getDefs = gets replDefs
+
+modifyDefs :: (Defs -> Defs) -> REPL ()
+modifyDefs f = modify (\o -> o { replDefs = f (replDefs o) })
+
+printDepth :: REPL ()
+printDepth = getDepth >>= lift . outputStrLn . show
 
 printDefs :: REPL ()
 printDefs = showDefs >>= lift . traverse_ outputStrLn
@@ -52,7 +85,7 @@ readDefs :: FilePath -> REPL ()
 readDefs name = readFileLines name >>= traverse_ definition
 
 showDefs :: REPL [String]
-showDefs = gets (fmap showDef . M.assocs)
+showDefs = fmap showDef . M.assocs <$> getDefs
 
 showDef :: (Ident, Term) -> String
 showDef (n, t) = n ++ " = " ++ render t
@@ -65,11 +98,11 @@ process input
 definition :: String -> REPL ()
 definition input = case parseTermDef input of
   Left err -> lift $ outputStrLn err
-  Right (n, t) -> modify (M.insert n t)
+  Right (n, t) -> modifyDefs (M.insert n t)
 
 unwrap :: Term -> REPL Term
 unwrap t = do
-  subs <- gets M.assocs
+  subs <- M.assocs <$> getDefs
   let t' = foldr (uncurry substitute) t subs
   if t' == t then pure t else unwrap t'
 
@@ -77,8 +110,9 @@ eval :: String -> REPL ()
 eval input = case parseTerm input of
   Left err -> lift $ outputStrLn err
   Right t  -> do
+    d <- getDepth
     t' <- unwrap t
-    lift $ traverse_ outputStrLn (reduceTerm "~ " 100 t')
+    lift $ traverse_ outputStrLn (reduceTerm "~ " d t')
 
 reduceTerm :: String -> Int -> Term -> [String]
 reduceTerm sep n = fmap ((sep ++) . render) . take n . reduceFully
